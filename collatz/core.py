8445c5272445390ef03b2f24d34c6eda074d5d51
mitchell.lee@showtime.net <mitchell.lee@showtime.net>
2023-12-12 15:58:16 -0800

    adding a recursive verion of Collatz

--- collatz/core.py
+++ collatz/core.py
@@ -30,3 +30,34 @@ def collatz_basic(n: int) -> List[int]:
         history.append(int(n))
 
     return history
+
+
+def collatz_recursive(n: int, seq: list) -> (int, List[int]):
+    """
+    A recursive Collatz conjecture function
+
+
+    Parameters
+    ----------
+    n: int
+        Our starting number
+    seq: List[Int]
+        The Collatz sequence that we have traversed up to this point.
+
+    Returns
+    -------
+    history: list[int]
+        The path it taken to get to one.
+
+    """
+
+    if n == 1:
+        return n, seq
+    elif (n % 2) == 0:
+        n = int(n / 2)
+        seq.append(n)
+        return collatz_recursive(n, seq)
+    else:
+        n = int(n * 3 + 1)
+        seq.append(n)
+        return collatz_recursive(n, seq)
--- tests/core_test.py
+++ tests/core_test.py
@@ -1,4 +1,4 @@
-from collatz.core import collatz_basic
+from collatz.core import collatz_basic, collatz_recursive
 
 
 class TestCollatzBasic:
@@ -10,4 +10,16 @@ class TestCollatzBasic:
     def test_16(self, seq_16):
         expected = seq_16
         actual = collatz_basic(16)
+        assert actual == expected
+
+
+class TestCollatzRecursive:
+    def test_15(self, seq_15):
+        expected = seq_15
+        _, actual = collatz_recursive(15, [15])
+        assert actual == expected
+
+    def test_16(self, seq_16):
+        expected = seq_16
+        _, actual = collatz_recursive(16, [16])
         assert actual == expected
