90f9c9a07ca35d7136a73c2f88ee5c6868a9162a
mitchell.lee@showtime.net <mitchell.lee@showtime.net>
2023-12-13 14:37:53 -0800

    added batch processing that uses caching of previous sequences

--- collatz/core.py
+++ collatz/core.py
@@ -1,7 +1,8 @@
-from typing import List, Callable, Iterable
+from typing import List, Callable, Iterable, Dict
+from copy import copy
 
 
-def collatz_basic(n: int) -> List[int]:
+def collatz_looping(n: int) -> List[int]:
     """
     A basic while loop implementation of Collatz conjecture function.
 
@@ -45,7 +46,7 @@ def collatz_recursive(n: int, seq: list = None) -> (int, List[int]):
 
     Returns
     -------
-    history: list[int]
+    seq: list[int]
         The path it taken to get to one.
 
     """
@@ -88,3 +89,86 @@ def batch_runner(func: Callable, batch: Iterable):
         seq_list.append(func(val))
 
     return seq_list
+
+
+# version where we take advantage of pre-computed history
+
+
+def collatz_looping_with_lookup(
+    n: int, seq_log: Dict[int, List[int]] = None
+) -> List[int]:
+    """
+    A basic while loop implementation of Collatz conjecture function.
+
+    Parameters
+    ----------
+    n: int
+        Our starting number
+    seq_log: Dict[int, List[int]]
+        Collatz sequences that we have computed for other numbers.
+        Once we hit a value in our lookup, we know the rest of the sequence.
+
+    Returns
+    -------
+    sequence: list[int]
+        The path it taken to get to one.
+    seq_log: Dict[int, List[int]]
+        Our lookup dictionary with the sequence for n added.
+    """
+
+    if seq_log is None:
+        seq_log = {}
+
+    input_n = copy(n)
+    sequence = [n]
+
+    while n != 1:
+        n_seq = seq_log.get(n)
+        if n_seq is not None:
+            # once if n is our log, we can
+            # just append the sequence for n
+            # and we are done
+            sequence += n_seq[1:]
+            break
+
+        if (n % 2) == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+
+        sequence.append(int(n))
+
+    seq_log[input_n] = sequence
+
+    return seq_log
+
+
+def batch_runner_with_lookup(
+    func: Callable, batch: Iterable, seq_log: Dict[int, List[int]] = None
+):
+    """
+    Run the Collatz sequence for a batch of numbers
+
+    Parameters
+    ----------
+    func: Callable
+        the function we will use to generate the Collatz sequence
+    seq: Interable
+        A list of values for which we want to compute the Collatz sequence
+    seq_log: Dict[int, List[int]]
+        A lookup of pre-commputed Collatz sequences
+
+    Returns
+    -------
+    seq_log:  Dict[int, List[int]]
+        A dictionary containing all of the computed Collatz sequences
+
+    """
+
+    if seq_log is None:
+        seq_log = {}
+
+    for val in batch:
+        seq_log = func(val, seq_log)
+
+    return seq_log
--- tests/core_test.py
+++ tests/core_test.py
@@ -1,15 +1,21 @@
-from collatz.core import collatz_basic, collatz_recursive, batch_runner
+from collatz.core import (
+    collatz_looping,
+    collatz_looping_with_lookup,
+    collatz_recursive,
+    batch_runner,
+    batch_runner_with_lookup,
+)
 
 
-class TestCollatzBasic:
+class TestCollatzLooping:
     def test_15(self, seq_15):
         expected = seq_15
-        actual = collatz_basic(15)
+        actual = collatz_looping(15)
         assert actual == expected
 
     def test_16(self, seq_16):
         expected = seq_16
-        actual = collatz_basic(16)
+        actual = collatz_looping(16)
         assert actual == expected
 
 
@@ -27,6 +33,36 @@ class TestCollatzRecursive:
 
 def test_batch_runner(seq_15, seq_16):
     expected = [seq_15, seq_16]
-    actual = batch_runner(collatz_basic, [15, 16])
+    actual = batch_runner(collatz_looping, [15, 16])
+
+    assert actual == expected
+
+
+# Test code where we taked advantage of pre-computed history
+class TestCollatzLoopingWithLookup:
+    def test_15(self, seq_15):
+        actual = collatz_looping_with_lookup(15)
+        expected = {15: seq_15}
+        assert actual == expected
+
+    def test_16_without_helpful_lookup(self, seq_15, seq_16):
+        seq_log = {15: seq_15}
+        actual = collatz_looping_with_lookup(16, seq_log)
+        expected = {15: seq_15, 16: seq_16}
+        assert actual == expected
+
+    def test_16_with_helpful_lookup(self, seq_15, seq_16):
+        seq_log = {8: [8, 4, 2, 1], 15: seq_15}
+        actual = collatz_looping_with_lookup(16, seq_log)
+        expected = {8: [8, 4, 2, 1], 15: seq_15, 16: seq_16}
+        assert actual == expected
+
+
+def test_batch_runner_with_lookup(seq_15, seq_16):
+    expected = {8: [8, 4, 2, 1], 15: seq_15, 16: seq_16}
+
+    actual = batch_runner_with_lookup(
+        collatz_looping_with_lookup, batch=[15, 16], seq_log={8: [8, 4, 2, 1]}
+    )
 
     assert actual == expected
